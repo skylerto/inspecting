b2e67c4f03565ad12086002356fdeb9d0aa129ba
Skyler Layne <skylerl@indellient.com>
2017-07-14 09:07:18 -0400

    stubbed it up

--- main.go
+++ main.go
@@ -2,91 +2,46 @@ package main
 
 import (
   "encoding/json"
-	"fmt"
+	// "fmt"
+  "html/template"
+  "net/http"
 	"io/ioutil"
+	"io"
 	"log"
-  "os"
+  // "os"
 )
 
-type Results struct {
-	Version string `json:"version"`
-	Controls []struct {
-		Status string `json:"status"`
-		CodeDesc string `json:"code_desc"`
-		RunTime float64 `json:"run_time"`
-		StartTime string `json:"start_time"`
-		Message string `json:"message,omitempty"`
-		SkipMessage string `json:"skip_message,omitempty"`
-		Resource string `json:"resource,omitempty"`
-	} `json:"controls"`
-	OtherChecks []interface{} `json:"other_checks"`
-	Profiles []struct {
-		Name string `json:"name"`
-		Title string `json:"title"`
-		Maintainer string `json:"maintainer"`
-		Copyright string `json:"copyright"`
-		CopyrightEmail string `json:"copyright_email"`
-		License string `json:"license"`
-		Summary string `json:"summary"`
-		Version string `json:"version"`
-		Supports []struct {
-			OsFamily string `json:"os-family"`
-		} `json:"supports"`
-		Controls []struct {
-			Title string `json:"title"`
-			Desc string `json:"desc"`
-			Impact float64 `json:"impact"`
-			Refs []interface{} `json:"refs"`
-			Tags struct {
-			} `json:"tags"`
-			Code string `json:"code"`
-			SourceLocation struct {
-				Ref string `json:"ref"`
-				Line int `json:"line"`
-			} `json:"source_location"`
-			ID string `json:"id"`
-			Results []struct {
-				Status string `json:"status"`
-				CodeDesc string `json:"code_desc"`
-				RunTime float64 `json:"run_time"`
-				StartTime string `json:"start_time"`
-			} `json:"results"`
-		} `json:"controls"`
-		Groups []struct {
-			Title interface{} `json:"title"`
-			Controls []string `json:"controls"`
-			ID string `json:"id"`
-		} `json:"groups"`
-		Attributes []struct {
-			Name string `json:"name"`
-			Options struct {
-				Default string `json:"default"`
-				Description string `json:"description"`
-			} `json:"options"`
-		} `json:"attributes"`
-		Sha256 string `json:"sha256"`
-	} `json:"profiles"`
-	Platform struct {
-		Name string `json:"name"`
-		Release string `json:"release"`
-	} `json:"platform"`
-	Statistics struct {
-		Duration float64 `json:"duration"`
-	} `json:"statistics"`
+func handler(w http.ResponseWriter, r *http.Request) {
+  log.Println("GET Request made for /view")
+  res := getResults("./linux.json")
+  t, _ := template.ParseFiles("templates/view.html")
+  t.Execute(w, res.Profiles)
+}
+
+func generate(w http.ResponseWriter, r *http.Request) {
+  log.Println("POST Request made for /generate")
+  body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+  if err != nil {
+      panic(err)
+  }
+  if err := r.Body.Close(); err != nil {
+      panic(err)
+  }
+  res := Results{}
+  json.Unmarshal(body, &res);
+  t, _ := template.ParseFiles("templates/view.html") //setp 1
+  t.Execute(w, res.Profiles) //step 2
 }
 
 func main() {
-  file := os.Args[1]
+  // file := os.Args[1]
 
-	b, err := ioutil.ReadFile(file)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// fmt.Printf("%s", b)
-
-  res := Results{}
-  json.Unmarshal([]byte(b), &res)
-  fmt.Println(res)
-
+  server := http.Server{
+    Addr: "127.0.0.1:8080",
+  }
+  fs := http.FileServer(http.Dir("static"))
+  http.Handle("/static/", http.StripPrefix("/static/", fs))
+  http.HandleFunc("/view", handler)
+  http.HandleFunc("/generate", generate)
+  server.ListenAndServe()
 }
---
+++ results.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+  "encoding/json"
+// 	"fmt"
+	"io/ioutil"
+	"log"
+//   "os"
+)
+
+type Results struct {
+	Version string `json:"version"`
+	Controls []struct {
+		Status string `json:"status"`
+		CodeDesc string `json:"code_desc"`
+		RunTime float64 `json:"run_time"`
+		StartTime string `json:"start_time"`
+		Message string `json:"message,omitempty"`
+		SkipMessage string `json:"skip_message,omitempty"`
+		Resource string `json:"resource,omitempty"`
+	} `json:"controls"`
+	OtherChecks []interface{} `json:"other_checks"`
+	Profiles []struct {
+		Name string `json:"name"`
+		Title string `json:"title"`
+		Maintainer string `json:"maintainer"`
+		Copyright string `json:"copyright"`
+		CopyrightEmail string `json:"copyright_email"`
+		License string `json:"license"`
+		Summary string `json:"summary"`
+		Version string `json:"version"`
+		Supports []struct {
+			OsFamily string `json:"os-family"`
+		} `json:"supports"`
+		Controls []struct {
+			Title string `json:"title"`
+			Desc string `json:"desc"`
+			Impact float64 `json:"impact"`
+			Refs []interface{} `json:"refs"`
+			Tags struct {
+			} `json:"tags"`
+			Code string `json:"code"`
+			SourceLocation struct {
+				Ref string `json:"ref"`
+				Line int `json:"line"`
+			} `json:"source_location"`
+			ID string `json:"id"`
+			Results []struct {
+				Status string `json:"status"`
+				CodeDesc string `json:"code_desc"`
+				RunTime float64 `json:"run_time"`
+				StartTime string `json:"start_time"`
+			} `json:"results"`
+		} `json:"controls"`
+		Groups []struct {
+			Title interface{} `json:"title"`
+			Controls []string `json:"controls"`
+			ID string `json:"id"`
+		} `json:"groups"`
+		Attributes []struct {
+			Name string `json:"name"`
+			Options struct {
+				Default string `json:"default"`
+				Description string `json:"description"`
+			} `json:"options"`
+		} `json:"attributes"`
+		Sha256 string `json:"sha256"`
+	} `json:"profiles"`
+	Platform struct {
+		Name string `json:"name"`
+		Release string `json:"release"`
+	} `json:"platform"`
+	Statistics struct {
+		Duration float64 `json:"duration"`
+	} `json:"statistics"`
+}
+
+func getResults(file string) Results {
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+  res := Results{}
+  json.Unmarshal([]byte(b), &res)
+  return res
+}
